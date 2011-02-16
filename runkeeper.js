8c36afcac69a12bf34cad889b71ea8b57d7b72df
phy9pas <phy9pas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-16 13:27:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/runkeeper-plugin/tags/1.1@346410 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- runkeeper.js
+++ runkeeper.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){ 
-	if ($('#runkeeper').length>0) {
-		url = $('#runkeeper').attr('title');
-		html = '';
-		html += '<iframe id="runkeeper_if" scrolling=no src="'+url+'"/>';
-		url = $('#runkeeper').html(html);
+
+var runkeeper = {
+	alreadyrunflag:0,
+	
+	init:function() {
+		if (document.addEventListener)
+  			document.addEventListener("DOMContentLoaded", function(){ runkeeper.alreadyrunflag=1; runkeeper.begin()}, false)			
+		else if (document.all && !window.opera){
+		  document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"><\/script>')
+		  var contentloadtag=document.getElementById("contentloadtag")
+		  contentloadtag.onreadystatechange=function(){
+			if (this.readyState=="complete"){
+			  runkeeper.alreadyrunflag=1
+			  runkeeper.begin()
+			}
+		  }
+		}	
+		
+		window.onload=function(){
+		  setTimeout("if (!runkeeper.alreadyrunflag) runkeeper.begin()", 500)
+		}		
+	},
+	
+	begin:function() {
+		if (document.getElementById('runkeeper')) {
+			url = document.getElementById('runkeeper').title;
+			html = '';
+			html += '<iframe id="runkeeper_if" scrolling=no src="'+url+'"/>';
+			document.getElementById('runkeeper').innerHTML = html;
+		}
 	}
-});
+}
+
+runkeeper.init();
